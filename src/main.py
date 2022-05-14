3c0ed9223b0de459723b399df7ae0a6edbbec4e1
Adrian Grosch <adrian.grosch@singular.inf.br>
2022-05-13 22:55:03 -0300

    limpeza de codigo

--- src/main.py
+++ src/main.py
@@ -1,19 +1,41 @@
+import statistics
+
 from src.a_star_agent import AStarAgent
 from src.deep_agent import DeepAgent
+from src.dummy_agent import DummyAgent
 from src.environment import Environment
-# from src.smart_agent import SmartAgent
+
+import sys
+
 from src.uniform_cost_agent import UniformCostAgent
 from src.wide_agent import WideAgent
 
-import sys
 sys.setrecursionlimit(10000)
-env = Environment(5)
-agent = UniformCostAgent(env)
+def run_with_coverage(agent_class):
+    stats=[]
+    print('--------------------')
+    for _ in range(1000):
+        env = Environment(_)
+        print(f"{_}--> {env.hand}")
+        agent = agent_class(env)
+        while not env.finish():
+            agent.step()
+        stats.append(env.score())
 
-while not env.finish():
-    agent.step()
+    print()
+    print(f'---------{agent_class}----------------')
+    print(f'Number of samples: {len(stats)}')
+    print(f'Wins: {sum([1 for x in stats if x >= 0])}')
+    print(f'Losses: {sum([1 for x in stats if x < 0])}')
+    print(f'Mean score: {round(statistics.mean(stats), 2)}')
+    print(f'Standard deviation (score): {round(statistics.stdev(stats), 2)}')
+    print(f'Median score: {round(statistics.median(stats), 2)}')
+    print('--------------------')
 
 
 
-print(env.score())
-print('adrian')
+run_with_coverage(DummyAgent)
+run_with_coverage(DeepAgent)
+run_with_coverage(AStarAgent)
+run_with_coverage(WideAgent)
+run_with_coverage(UniformCostAgent)
