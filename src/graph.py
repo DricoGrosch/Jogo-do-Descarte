aa842cac5bada477135ec3f2562070fe9940316f
Adrian Grosch <adrian.grosch@singular.inf.br>
2022-05-15 17:19:09 -0300

    verificação para adicionar nós a visitar

--- src/graph.py
+++ src/graph.py
@@ -12,6 +12,13 @@ class Graph:
         self.open_nodes = []
         self.current_node = None
 
+    def is_new_node(self, node):
+        for known_node in [*self.visited_nodes,*self.open_nodes]:
+            if node['thrown_card'] == known_node['thrown_card'] and node['environment'].hand == known_node[
+                'environment'].hand:
+                return False
+        return True
+
     def add_open_nodes(self, nodes):
         self.open_nodes.extend(nodes)
 
@@ -27,13 +34,15 @@ class Graph:
                 next_node_environment.act(card)
                 next_node_environment.winning_track.append(card)
                 next_node = {'thrown_card': card, 'environment': next_node_environment}
-                node_neighbors.append(next_node)
+                if self.is_new_node(next_node):
+                    node_neighbors.append(next_node)
         if not node_neighbors and current_stack:
             next_node_environment = current_environment._copy()
             next_node_environment.act(None)
             next_node_environment.winning_track.append(None)
             next_node = {'thrown_card': None, 'environment': next_node_environment}
-            node_neighbors.append(next_node)
+            if self.is_new_node(next_node):
+                node_neighbors.append(next_node)
         return node_neighbors
 
     def visit_node(self, node):
